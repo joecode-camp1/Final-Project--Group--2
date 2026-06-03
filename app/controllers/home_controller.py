0eee6dfaf2628d74601053ad3c70e19b68b68bdc
Christian <misterchristian4@gmail.com>
2026-06-03 18:42:33 +0000

    fix all bug in the code

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 home_bp = Blueprint("home",__name__)
-=======
+
 from flask import Blueprint, render_template, request, redirect, url_for
 home_bp = Blueprint('home', __name__)
 @home_bp.route('/')
@@ -10,7 +10,7 @@ def splash():
 @home_bp.route('/index.html')
 def index():
     return render_template('index.html')
->>>>>>> 9a170a78507a70e27bfb3891a6193573db7602d0
+
 
 
 @home_bp.route("/")
